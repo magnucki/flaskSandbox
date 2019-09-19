fff71664b5c4d9d2501cbf29ebe11f8639bd0ae2
Rico Magnucki <rico@icancode.de>
2019-09-19 11:05:06 +0200

    finished part 4 - databases

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app import app 
+"""Main microblog module"""
+from app import app, db 
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    """defines shell context for flask - no need for manual imports"""
+    return {'db': db, 'User': User, 'Post': Post}
